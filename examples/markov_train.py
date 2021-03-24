accc2cea68be4dc1dc7b9503684853bcdf124bab
jarbasal <jarbasai@mailfence.com>
2021-03-24 16:14:32 +0000

    #0 - hello mkovjson

---
+++ examples/markov_train.py
@@ -0,0 +1,21 @@
+from markovjson import MarkovCharJson
+from os.path import isfile
+
+# for names modelling at char level makes sense
+# word level could make more sense for things like song lyrics
+mkov = MarkovCharJson(order=4)
+model = "places"  # or "names"
+path = f"{model}_c{mkov.order}.mkovjson"
+
+
+if isfile(path):
+    mkov.load(path)
+else:
+    with open(f"datasets/{model}.txt") as f:
+        for line in f.readlines():
+            # TODO cleanup string ?
+            if not line.strip():
+                continue
+            mkov.add_string(line.strip())
+
+    mkov.save(path)
