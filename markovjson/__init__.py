52377aa4869193cfd5e72a6300dbf4ad5c4a1cc3
jarbasal <jarbasai@mailfence.com>
2021-03-24 18:09:52 +0000

    tokenizers.py

--- markovjson/__init__.py
+++ markovjson/__init__.py
@@ -1,3 +1,3 @@
 from markovjson.mkov import MarkovJson, ReverseMarkovJson
-from markovjson.generative import MarkovCharJson, ReverseMarkovWordJson, \
+from markovjson.tokenizers import MarkovCharJson, ReverseMarkovWordJson, \
     ReverseMarkovCharJson, MarkovWordJson
