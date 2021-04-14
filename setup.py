c3b67f45305d1392aab381622f24cd6f45ffcbda
jarbasal <jarbasai@mailfence.com>
2021-04-14 17:51:13 +0100

    nltk corpora

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='markovjson',
     version='0.1.0',
     packages=['markovjson'],
-    url='',
+    url='https://github.com/OpenJarbas/markovjson',
     license='apache-2',
     author='jarbasAi',
+    install_requires=["json_database", "nltk"],
     author_email='jarbasai@mailfence.com',
     description='json meets markov chains'
 )
